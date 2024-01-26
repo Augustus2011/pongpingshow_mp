1a6e94e23a4ac4f11bfd616b89e23435a36b8791
Augustus2011 <kun.kerdthaisong@gmail.com>
2024-01-26 20:34:07 +0700

    study kunn

---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+from _thread import *
+import _thread
+import os
+import sys
+
+server="192.168.1.4"
+port=8000
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+try:
+    s.bind((server,port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(2)
+print("waiting for a connection, server started")
+
+def threaded_client(conn):
+    reply=""
+    while True:
+        try:
+            data=conn.recv(2048)
+            reply=data.decode("utf-8")
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("recieved",reply)
+                print("sending :",reply)
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+            
+
+while True:
+    conn,addr=s.accept()
+    print("Connected to:",addr)
+    start_new_thread(threaded_client,(conn,)) 
